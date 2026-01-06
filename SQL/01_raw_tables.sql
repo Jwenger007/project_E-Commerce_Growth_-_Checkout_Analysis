648f07130a353ddf0a9844312da41252398f4bbb
Jwenger007 <brmdgkb2t5@privaterelay.appleid.com>
2026-01-06 12:06:01 +0100

    lil.change

---
+++ SQL/01_raw_tables.sql
@@ -0,0 +1,55 @@
+CREATE TABLE raw.orders (
+    order_id TEXT,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TIMESTAMP,
+    order_approved_at TIMESTAMP,
+    order_delivered_carrier_date TIMESTAMP,
+    order_delivered_customer_date TIMESTAMP,
+    order_estimated_delivery_date TIMESTAMP
+);
+
+CREATE TABLE raw.order_items (
+    order_id TEXT,
+    order_item_id INTEGER,
+    product_id TEXT,
+    seller_id TEXT,
+    shipping_limit_date TIMESTAMP,
+    price NUMERIC,
+    freight_value NUMERIC
+);
+
+CREATE TABLE raw.order_payments (
+    order_id TEXT,
+    payment_sequential INTEGER,
+    payment_type TEXT,
+    payment_installments INTEGER,
+    payment_value NUMERIC
+);
+
+CREATE TABLE raw.order_reviews (
+    review_id TEXT,
+    order_id TEXT,
+    review_score INTEGER,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
+
+CREATE TABLE raw.products (
+    product_id TEXT,
+    product_category_name TEXT,
+    product_name_lenght INTEGER,
+    product_description_lenght INTEGER,
+    product_photos_qty INTEGER,
+    product_weight_g INTEGER,
+    product_length_cm INTEGER,
+    product_height_cm INTEGER,
+    product_width_cm INTEGER
+);
+
+CREATE TABLE raw.product_category_translation (
+    product_category_name TEXT,
+    product_category_name_english TEXT
+);
